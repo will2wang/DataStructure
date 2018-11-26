33e8c4e2f04428cd718dde7be95249da53113e62
will2wang <45348814+will2wang@users.noreply.github.com>
2018-11-26 22:04:35 +0800

    Add files via upload

---
+++ 线性表.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  2-线性表
+//
+//  Created by Will Wang on 2018/11/26.
+//  Copyright © 2018 Will Wang. All rights reserved.
+//
+
+#include <stdio.h>
+
+const int Maxsize=100;
+typedef struct
+{
+    DataType data[Maxsize]；
+    int length;
+}SeqList;
+SeqList L;
+//
+
+const int Maxsize=7;
+typedef struct
+{
+    int num;
+    char name[8];
+    char sex[2];
+    int age;
+    int score;
+}DataType;
+typedef struct
+{
+    DataType data[Maxsize];
+    int length;
+}SeqList;
+SeqList student;
